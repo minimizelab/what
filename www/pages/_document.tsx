7d3c367268c6ff38f3a0e35981dd9af00d798ac7
Axel Ekwall <git@axelekwall.se>
2021-08-10 21:31:17 +0200

    add html lang attr

--- www/pages/_document.tsx
+++ www/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="sv">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500&display=swap"
