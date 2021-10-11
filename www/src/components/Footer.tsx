29222558adfc28688f9e056dff16cdef2867670a
Axel Ekwall <git@axelekwall.se>
2021-10-11 21:19:18 +0200

    basic footer styling

--- www/src/components/Footer.tsx
+++ www/src/components/Footer.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from 'react';
 
 const Footer: FC = () => (
-  <footer className="border border-red-500">Footer</footer>
+  <footer className="px-8">
+    <div className="flex flex-row border-t-2 border-white py-12">
+      <div className="flex flex-col mr-2">icon</div>
+      <div className="flex flex-col flex-1">Text</div>
+    </div>
+  </footer>
 );
 
 export default Footer;
