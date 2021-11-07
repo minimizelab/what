3a256b143cb1226bfe0621a161957524d0378b56
Axel Ekwall <git@axelekwall.se>
2021-11-07 18:21:21 +0100

    make project content images responsive

--- www/src/serializers.tsx
+++ www/src/serializers.tsx
@@ -1,22 +1,23 @@
+import { SanityImageAssetDocument } from '@sanity/client';
 import Image from 'next/image';
-import { urlFor } from './lib/sanity.client';
 
 export default {
   types: {
-    image: (props: any) => {
-      const src = urlFor(props.node.asset).url();
-      return (
-        src && (
-          <Image
-            src={src}
-            alt=""
-            layout="responsive"
-            width="300"
-            height="200"
-            objectFit="contain"
-          />
-        )
-      );
-    },
+    image: ({
+      node: { asset },
+    }: {
+      node: { asset: SanityImageAssetDocument };
+    }) => (
+      <Image
+        src={asset.url}
+        alt=""
+        placeholder={asset.metadata.lqip ? 'blur' : 'empty'}
+        layout="responsive"
+        width={asset.metadata.dimensions.width}
+        height={asset.metadata.dimensions.height}
+        objectFit="contain"
+        blurDataURL={asset.metadata.lqip}
+      />
+    ),
   },
 };
