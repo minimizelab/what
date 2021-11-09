e8ce4d12ad178443e05ef2fe336c19eac60f6323
Axel Ekwall <git@axelekwall.se>
2021-11-09 09:50:04 +0100

    add admin redirect

--- www/next.config.js
+++ www/next.config.js
@@ -6,6 +6,11 @@ module.exports = {
         destination: '/',
         permanent: true,
       },
+      {
+        source: '/admin/:p*',
+        destination: 'https://what-admin.minimize.se/',
+        permanent: true,
+      },
     ];
   },
   images: {
