20675d7f13e67b11bbba950c579f5910a9fc2933
Ella Wirstad G <ella.wg@hotmail.com>
2021-11-09 09:28:31 +0100

    Style header (#17)

--- www/src/components/organisms/Header.tsx
+++ www/src/components/organisms/Header.tsx
@@ -1,11 +1,21 @@
-import { FC } from 'react';
+import classNames from 'classnames';
+import React, { FC } from 'react';
+import H1 from '../atoms/H1';
 
 type Props = { title: string; className?: string };
 
 const Header: FC<Props> = ({ title, children, className }) => (
-  <header className={className}>
-    <h1>{title}</h1>
-    {children}
+  <header
+    className={classNames(
+      'h-60 border-b border-white flex flex-row justify-between',
+      className
+    )}
+  >
+    <div>{children}</div>
+    <div className="flex flex-col content-between text-right justify-between pb-8">
+      <H1>WHAT</H1>
+      <h1>{title}</h1>
+    </div>
   </header>
 );
 
