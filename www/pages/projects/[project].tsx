beb10deacb1ed84dee9dd6908e9589cbdbbe0091
Axel Ekwall <git@axelekwall.se>
2022-01-22 18:10:17 +0100

    generate rows with images

--- www/pages/projects/[project].tsx
+++ www/pages/projects/[project].tsx
@@ -1,3 +1,5 @@
+import { SanityImageAssetDocument } from '@sanity/client';
+import classNames from 'classnames';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Image from 'next/image';
 import React, { FC } from 'react';
@@ -14,6 +16,7 @@ type Props = {
 };
 
 const ProjectPage: FC<Props> = ({ project, settings }) => {
+  const imageRows = createImageRows(project.images.map((obj) => obj.asset));
   return (
     <Page settings={settings}>
       <ProjectHeader
@@ -22,25 +25,64 @@ const ProjectPage: FC<Props> = ({ project, settings }) => {
         description={project.description}
         subTitle={project.subTitle}
       />
-      <div className="mb-6">
-        {project.images.map(({ asset: { _id, url, metadata } }) => (
-          <div key={_id} className="mb-2 mt-4">
-            <Image
-              src={url}
-              alt=""
-              placeholder={metadata.lqip ? 'blur' : 'empty'}
-              layout="responsive"
-              width={metadata.dimensions.width}
-              height={metadata.dimensions.height}
-              objectFit="contain"
-              blurDataURL={metadata.lqip}
-            />
-          </div>
+      <div className="mb-6 flex flex-col">
+        {imageRows.map((row) => (
+          <ImageRow key={row[0]._id} images={row}></ImageRow>
         ))}
       </div>
       <p>{project.credits}</p>
     </Page>
   );
+};
+
+const ImageRow: FC<{ images: SanityImageAssetDocument[] }> = ({ images }) => (
+  <div className="flex flex-row justify-center items-center">
+    {images.map((img) => (
+      <div
+        key={img._id}
+        className={classNames(
+          'mb-2 mt-4',
+          isLandscape(img) ? 'w-full' : 'w-1/2'
+        )}
+      >
+        <Image
+          src={img.url}
+          alt=""
+          placeholder={img.metadata.lqip ? 'blur' : 'empty'}
+          layout="responsive"
+          width={img.metadata.dimensions.width}
+          height={img.metadata.dimensions.height}
+          objectFit="contain"
+          blurDataURL={img.metadata.lqip}
+        />
+      </div>
+    ))}
+  </div>
+);
+
+const isLandscape = (img: SanityImageAssetDocument) =>
+  img.metadata.dimensions.height < img.metadata.dimensions.width;
+
+const createImageRows = (images: SanityImageAssetDocument[]) => {
+  const rows: SanityImageAssetDocument[][] = [];
+  while (images.length) {
+    const firstImage = images.shift() as SanityImageAssetDocument;
+    if (isLandscape(firstImage)) {
+      rows.push([firstImage]);
+    } else {
+      if (images.length) {
+        const secondImage = images.shift() as SanityImageAssetDocument;
+        if (isLandscape(secondImage)) {
+          rows.push([firstImage], [secondImage]);
+        } else {
+          rows.push([firstImage, secondImage]);
+        }
+      } else {
+        rows.push([firstImage]);
+      }
+    }
+  }
+  return rows;
 };
 
 export const getStaticPaths: GetStaticPaths<Params> = async () => {
