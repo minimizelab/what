0fa5b9a0b89c4814448c67ea7db7919d338563f9
Axel Ekwall <git@axelekwall.se>
2022-02-08 14:00:04 +0100

    add contact information to studio page (#34)

--- www/pages/studio.tsx
+++ www/pages/studio.tsx
@@ -4,12 +4,13 @@ import { PortableText } from '@portabletext/react';
 import sanity from '../src/services/sanity';
 import { GetStaticProps } from 'next';
 import { revalidate } from '../src/config/defaults';
-import { Employee, Settings, Studio } from '../src/types';
+import { DefaultPageProps, Employee, Studio } from '../src/types';
 import EmployeeCard from '../src/components/molecules/EmployeeCard';
 import getSortedArray from '../src/utils/getSortedArray';
 import serializers from '../src/lib/serializers';
+import TextMedium from '../src/components/atoms/TextMedium';
 
-type Props = { employees: Employee[]; studio: Studio; settings: Settings };
+type Props = DefaultPageProps & { employees: Employee[]; studio: Studio };
 
 const Block: FC = ({ children }) => (
   <p className="lg:w-2/3 w-full text-3xl">{children}</p>
@@ -17,13 +18,55 @@ const Block: FC = ({ children }) => (
 
 const StudioPage: FC<Props> = ({ employees, studio, settings }) => (
   <Page className="pb-8" settings={settings}>
-    <div className="flex flex-row justify-start mb-16">
+    <div className="flex flex-col items-start">
       <PortableText
         value={studio.pageContent}
         components={{ ...serializers, block: Block }}
       />
+      <div className="flex flex-row mt-10 w-full">
+        <TextMedium className="font-medium w-32">Telefon</TextMedium>
+        <TextMedium className="flex-1">
+          <a
+            className="hover:text-what-brick"
+            href={`callto:${settings.phone}`}
+          >
+            {settings.phone}
+          </a>
+        </TextMedium>
+      </div>
+      <div className="flex flex-row mt-4 w-full">
+        <TextMedium className="font-medium w-32">Mail</TextMedium>
+        <TextMedium className="flex-1">
+          <a
+            className="hover:text-what-brick"
+            href={`mailto:${settings.email}`}
+          >
+            {settings.email}
+          </a>
+        </TextMedium>
+      </div>
+      <div className="flex flex-row mt-4 w-full">
+        <TextMedium className="font-medium w-32">Address</TextMedium>
+        <TextMedium className="flex flex-col flex-1">
+          {settings.address.map((line) => (
+            <span key={line}>{line}</span>
+          ))}
+        </TextMedium>
+      </div>
+      <div className="flex flex-row mt-4 w-full">
+        <TextMedium className="font-medium w-32">Jobb</TextMedium>
+        <TextMedium className="flex-1">
+          {'Skicka CV & portfolio till '}
+          <a
+            className="hover:text-what-brick"
+            href={`mailto:${settings.emailJob}`}
+          >
+            {settings.emailJob}
+          </a>
+        </TextMedium>
+      </div>
     </div>
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+    <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
       {employees.map((employee) => (
         <EmployeeCard key={employee.email} employee={employee}></EmployeeCard>
       ))}
--- www/src/components/organisms/Footer.tsx
+++ www/src/components/organisms/Footer.tsx
@@ -1,15 +1,12 @@
 import React, { FC } from 'react';
 import Section from '../atoms/Section';
 
-const Footer: FC = () => (
+const Footer: FC<{ email: string }> = ({ email }) => (
   <footer>
     <Section className="flex flex-row justify-between py-12 flex-wrap">
       <div className="text-3xl">what! arkitektur</div>
-      <a
-        className="text-3xl hover:text-what-brick"
-        href="mailto:hello@whats.se"
-      >
-        info@whats.se
+      <a className="text-3xl hover:text-what-brick" href={'mailto:' + email}>
+        {email}
       </a>
     </Section>
   </footer>
