711893b9472bb8ba7ffc01dbf7dbb902fb750fc3
Ella Wirstad <ella.wg@hotmail.com>
2022-10-22 18:42:59 +0200

    Update text sizes, margins and dot size

--- www/src/components/organisms/Nav.tsx
+++ www/src/components/organisms/Nav.tsx
@@ -3,12 +3,12 @@ import { FC } from 'react';
 
 const Nav: FC = () => (
   <nav className="flex flex-col lg:flex-row space-x-6 h-full items-end justify-end">
-    <p className="sm:text-3xl text-2xl hover:text-what-brick">
+    <p className="md:text-3xl text-2xl hover:text-what-brick">
       <Link href="/">
         <a className="cursor-pointer">projekt</a>
       </Link>
     </p>
-    <p className="sm:text-3xl text-2xl hover:text-what-brick">
+    <p className="md:text-3xl text-2xl hover:text-what-brick">
       <Link href="/studio">
         <a className="cursor-pointer">studio</a>
       </Link>
