cb894ba561ac2086d51965f30989c22cb231af55
Axel Ekwall <git@axelekwall.se>
2022-11-02 19:46:43 +0100

    Remove extra div from markup

--- www/src/components/molecules/ProjectCard.tsx
+++ www/src/components/molecules/ProjectCard.tsx
@@ -12,34 +12,32 @@ type Props = {
 const ProjectCard: FC<Props> = ({ project, prio = false }) => {
   const [hover, setHover] = useState<boolean>(false);
   return (
-    <div>
-      <Link href={`/projects/${project.path.current}`} key={project._id}>
-        <a className="flex flex-col cursor-pointer">
-          <div
-            className="relative flex-1"
-            onMouseEnter={() => setHover(true)}
-            onMouseLeave={() => setHover(false)}
-          >
-            {project.mainImage && (
-              <SanityImage
-                img={project.mainImage}
-                sizes="(min-width: 1280px) 45vw, (min-width: 1024px) 67vw, 134vw"
-                layout="responsive"
-                width="4"
-                height="3"
-                objectFit="cover"
-                priority={prio}
-                objectPosition="center"
-                alt={'image for project ' + project.title}
-              />
-            )}
-          </div>
+    <div className="flex flex-col">
+      <Link href={`/projects/${project.path.current}`}>
+        <a
+          className="relative flex-1 cursor-pointer"
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+        >
+          {project.mainImage && (
+            <SanityImage
+              img={project.mainImage}
+              sizes="(min-width: 1280px) 45vw, (min-width: 1024px) 67vw, 134vw"
+              layout="responsive"
+              width="400"
+              height="300"
+              objectFit="cover"
+              priority={prio}
+              objectPosition="center"
+              alt={'image for project ' + project.title}
+            />
+          )}
         </a>
       </Link>
-      <Link href={`/projects/${project.path.current}`} key={project._id}>
+      <Link href={`/projects/${project.path.current}`}>
         <a
           className={classNames(
-            'text-xl pt-2 inline-block hover:text-what-brick cursor-pointer',
+            'text-xl pt-2 self-start hover:text-what-brick cursor-pointer',
             hover && 'text-what-brick'
           )}
         >
