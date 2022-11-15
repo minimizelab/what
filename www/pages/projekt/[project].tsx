0b8c5cf419b00cdf8b0eb932d3296fd49b174389
Axel Ekwall <git@axelekwall.se>
2022-11-15 21:30:25 +0100

    Add OG tags

--- www/pages/projekt/[project].tsx
+++ www/pages/projekt/[project].tsx
@@ -1,9 +1,12 @@
+import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { FC } from 'react';
 import { ImageGrid } from '../../src/components/molecules/ImageGrid';
 import ProjectHeader from '../../src/components/organisms/ProjectHeader';
 import Page from '../../src/components/templates/Page';
+import { imageBuilder } from '../../src/lib/imageBuilder';
 import sanity from '../../src/services/sanity';
 import { Project, Settings } from '../../src/types';
 
@@ -16,8 +19,22 @@ type Props = {
 const ProjectPage: FC<Props> = ({ project, settings }) => {
   const images = project.images.slice(1).map((obj) => obj.asset);
   const router = useRouter();
+  const ogImageSrc = imageBuilder(project.mainImage as SanityImageSource)
+    .width(1200)
+    .height(630)
+    .url();
   return (
     <Page settings={settings} className="items-center">
+      <Head>
+        <title>{project.title}</title>
+        <meta
+          property="og:url"
+          content={`https://www.whats.se/projekt/${project.path.current}`}
+        ></meta>
+        <meta property="og:image" content={ogImageSrc} />
+        <meta property="og:title" content={project.title} />
+        <meta property="og:type" content="website" />
+      </Head>
       <article className="w-full max-w-7xl flex flex-col">
         <ProjectHeader project={project} />
         <ImageGrid images={images} />
