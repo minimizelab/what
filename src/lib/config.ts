694e83e3dabd7d2f1cccfe8255aa1e6342da0f0a
Axel Ekwall <git@axelekwall.se>
2024-02-07 15:52:02 +0100

    update sanity

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,7 +3,7 @@ import { ClientConfig } from 'next-sanity';
 const config: ClientConfig = {
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: '2021-06-06',
+  apiVersion: '2024-02-07',
   useCdn: false,
 };
 
