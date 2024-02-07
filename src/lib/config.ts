196da60a5f046b602c5a76a162132a0ea6e11ef9
Axel Ekwall <git@axelekwall.se>
2024-02-07 15:38:08 +0100

    move www to root

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,6 +4,7 @@ const config: ClientConfig = {
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: '2021-06-06',
+  useCdn: false,
 };
 
 export default config;
